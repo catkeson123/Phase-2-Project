cd69890d4f63ac90af367305e9b8af808a88ca55
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-03-06 16:20:55 -0500

    generate random beer fact

--- phase-2-project/src/Home.js
+++ phase-2-project/src/Home.js
@@ -1,9 +1,25 @@
-function Home () {
-    return (
-        <div>
-            <h1>Home HOLDER</h1>
-        </div>
-    )
+import { useState } from "react";
+
+function Home() {
+  const [fact, setFact] = useState("");
+
+  let randomFact = 0;
+
+  const handleClick = () => {
+    randomFact = Math.floor(Math.random() * 15);
+
+    fetch("http://localhost:6001/facts")
+      .then((res) => res.json())
+      .then((data) => setFact(data[randomFact].fact));
+  };
+
+  return (
+    <div>
+      <h1>HOME PAGE</h1>
+      <button onClick={handleClick}>Generate Beer Fact</button>
+      <h4>{fact}</h4>
+    </div>
+  );
 }
 
 export default Home;
